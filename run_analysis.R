432c6f57cdb81d8238faf0c5952f71c32de15520
CHENYANG ZHAO <ianchz@CHENYANGs-MacBook-Pro.local>
2015-06-21 11:47:43 -0700

    modified script to run in repo

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,3 @@
-# create a directory to save samsung data
-if (!file.exists('./samsung')){
-  dir.create('./samsung')
-}
-
 # download data set name it data.zip
 #download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', destfile = './data.zip', method='curl')
 # unzip data into samsung folder, overwrite existing data
@@ -10,14 +5,14 @@ if (!file.exists('./samsung')){
 
 # merge training and testing dataset
 # 1. read training data
-x_train <- read.table('./samsung/UCI HAR Dataset/train/X_train.txt')
-y_train <- read.table('./samsung/UCI HAR Dataset/train/Y_train.txt')
-subject_train <- read.table('./samsung/UCI HAR Dataset/train/subject_train.txt')
+x_train <- read.table('./UCI HAR Dataset/train/X_train.txt')
+y_train <- read.table('./UCI HAR Dataset/train/Y_train.txt')
+subject_train <- read.table('./UCI HAR Dataset/train/subject_train.txt')
 
 # 2. read testing data
-x_test <- read.table('./samsung/UCI HAR Dataset/test/X_test.txt')
-y_test <- read.table('./samsung/UCI HAR Dataset/test/Y_test.txt')
-subject_test <- read.table('./samsung/UCI HAR Dataset/test/subject_test.txt')
+x_test <- read.table('./UCI HAR Dataset/test/X_test.txt')
+y_test <- read.table('./UCI HAR Dataset/test/Y_test.txt')
+subject_test <- read.table('./UCI HAR Dataset/test/subject_test.txt')
 
 # 1. merge those data tables!
 x_all <- rbind(x_train, x_test) # all the feature sets
@@ -26,7 +21,7 @@ subject_all <- rbind(subject_train, subject_test)
 
 # extract mean and standard deviation, giving descriptive names to column and rows
 # acquire features label as column names
-features <- read.table('./samsung/UCI HAR Dataset/features.txt')
+features <- read.table('./UCI HAR Dataset/features.txt')
 mean_std_index <- grep('-(mean|std)\\(\\)', features[, 2])
 mean_std <- features[mean_std_index, ]
 # subset x_all using mean_std_index
@@ -35,7 +30,7 @@ x_all <- x_all[ , mean_std[, 1]]
 names(x_all) <- mean_std[, 2]
 
 # read activity labels
-activity_labels <- read.table('./samsung/UCI HAR Dataset/activity_labels.txt')
+activity_labels <- read.table('./UCI HAR Dataset/activity_labels.txt')
 # rename lable to be more descriptive, 
 y_all[ , 1] <- activity_labels[y_all[, 1], 2]
 # rename lable column names
